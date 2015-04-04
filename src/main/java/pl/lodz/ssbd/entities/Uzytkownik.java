17aee6f1f0bd2eee1c59f2adea6136f967657e37
Robert Mielczarek <180640@edu.p.lodz.pl>
2015-04-04 20:32:32 +0200

    Revert "SSBDV-58 Dodanie dwoch brakujacych constrainow"
    This reverts commit 9c4b7db2cb193780ba814c58fcd4a56e9bb762e4.

--- src/main/java/pl/lodz/ssbd/entities/Uzytkownik.java
+++ src/main/java/pl/lodz/ssbd/entities/Uzytkownik.java
@@ -69,13 +69,13 @@ public class Uzytkownik implements Serializable {
     @Basic(optional = false)
     @NotNull
     @Size(min = 1, max = 50)
-    @Column(unique = true, nullable = false, length = 50)
+    @Column(nullable = false, length = 50)
     private String login;
     // @Pattern(regexp="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", message="Invalid email")//if the field contains email address consider using this annotation to enforce field validation
     @Basic(optional = false)
     @NotNull
     @Size(min = 1, max = 50)
-    @Column(unique = true, nullable = false, length = 50)
+    @Column(nullable = false, length = 50)
     private String email;
     @Column(name = "czas_pop_zal", insertable = false)
     @Temporal(TemporalType.TIMESTAMP)
