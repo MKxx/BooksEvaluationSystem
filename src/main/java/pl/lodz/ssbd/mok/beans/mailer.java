401b3dfdf584bcd4437562753aada1f19b597251
180582 <180582@edu.p.lodz.pl>
2015-04-26 19:42:56 +0200

    Dodano funkcje wysyłania maili po rejestracji, oraz poprawiono mailera

---
+++ src/main/java/pl/lodz/ssbd/mok/beans/mailer.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.lodz.ssbd.mok.beans;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.internet.MimeMessage.RecipientType;
+import javax.mail.util.ByteArrayDataSource;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+
+
+/**
+ *
+ * @author Jakub Kępa 180582
+ */
+
+public class mailer {
+
+      
+
+
+
+
+ public void wyslij(String email, String temat, String tresc)
+ {   
+  try{
+   InitialContext ctx = new InitialContext();
+   Session sesja;
+   sesja = (Session) ctx.lookup("mail/mailer");
+
+
+
+  MimeMessage wiadomosc = new MimeMessage(sesja);
+  //wiadomosc.setFrom(new InternetAddress(this));
+  wiadomosc.addRecipient(Message.RecipientType.TO, new InternetAddress(email));
+  wiadomosc.setSubject(temat);
+  wiadomosc.setText(tresc);
+  Transport.send(wiadomosc);
+  }
+  catch(AddressException a){
+      Logger.getLogger("Bład adresu");
+  }
+  catch(MessagingException a){
+      Logger.getLogger("Bład wysyłania");
+  }  
+  catch (NamingException ex) {
+      Logger.getLogger("Bład NamingException");
+     }
+  Logger.getLogger("Wysłano");
+ }
+ public void wyslijPoZarejestrowaniu(String email, String login, String haslo){
+     String temat="Potwierdzenie rejestracji w naszym przecudnym serwisie";
+     String tresc="Dziekujemy za rejestracje! \nTwoje konto musi zostać potwierdzone przez administratora \nJak na razie twój login to: "+login+"\nA twoje haslo to: "+ haslo+"\nŻyczymy miłego dnia (chociaż, nim administrator potwierdzi twoje konto to warto już życzyć  co najmniej szczęśliwego nowego roku).";
+     wyslij(email, temat, tresc);
+ }
+ public void wyslijPrzykladowegoMaila(){
+        wyslijPoZarejestrowaniu("kubakepa@wp.pl","aaa", "aaa");
+
+ }
+}
+
+ 
+
