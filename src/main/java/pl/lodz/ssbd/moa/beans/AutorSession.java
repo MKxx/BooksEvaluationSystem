cfcba20f9b6744f05012b90bd95de16a2bba494d
mchmielecka <180513@edu.p.lodz.pl>
2015-05-20 19:09:14 +0200

    SSBDV-261-zaimplementowac-wyswietlanie-listy-autorow

---
+++ src/main/java/pl/lodz/ssbd/moa/beans/AutorSession.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.lodz.ssbd.moa.beans;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.security.RolesAllowed;
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import pl.lodz.ssbd.entities.Autor;
+import pl.lodz.ssbd.moa.endpoints.MOAEndpointLocal;
+
+/**
+ *
+ * @author Marta
+ */
+@SessionScoped
+public class AutorSession implements Serializable {
+
+    @EJB
+    MOAEndpointLocal MOAEndpoint;
+    
+    
+    
+    public AutorSession() {
+    }
+ 
+    @RolesAllowed("PrzegladanieAutorow")
+    public List<Autor> pobierzListeAutorow(){
+        return MOAEndpoint.pobierzListeAutorow(); 
+    }
+    
+}
--- src/main/java/pl/lodz/ssbd/moa/endpoints/MOAEndpoint.java
+++ src/main/java/pl/lodz/ssbd/moa/endpoints/MOAEndpoint.java
@@ -32,7 +32,7 @@ public class MOAEndpoint implements MOAEndpointLocal {
     @Override
     @PermitAll
     public List<Autor> pobierzListeAutorow() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+      return AutorFacade.findAll();
     }
 
     @Override
